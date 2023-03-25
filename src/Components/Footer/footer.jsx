c2efb79fdcb4c3ae7403b6b6379603374e20a6cf
Tornuz <yadavgourav2001@gmail.com>
2023-03-25 11:46:27 +0530

    first commit

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,32 +1,153 @@
-import React from 'react';
-import './footer.css';
-import video from '../../Assets/ocean.mp4';
-import { FiSend } from 'react-icons/fi';
+import React, { useEffect } from "react";
+import "./footer.css";
+import video from "../../Assets/ocean.mp4";
+import { FiChevronRight, FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import {
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const Footer = () => {
-    return (
-        <section className="footer">
-            <div className="videoDiv">
-            <video src={video} muted autoPlay loop typeof="video/mp4"></video>
+
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  }, [])
+
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video} muted autoPlay loop typeof="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div data-aos="fade-up" className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div data-aos="fade-up" className="inputDiv flex">
+            <input type="text" placeholder="Enter Email here.." />
+            <button data-aos="fade-up" className="btn flex" type="submit">
+              SEND
+              <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" />
+              </a>
             </div>
 
-            <div className="secContent container">
-                <div className="contactDiv flex">
-                    <div className="text">
-                        <small>KEEP IN TOUCH</small>
-                        <h2>Travel with us</h2>
-                    </div>
-
-                    <div className="inputDiv flex">
-                        <input type="text" placeholder='Enter Email here..'/>
-                        <button className="btn flex" type='submit'>
-                            SEND<FiSend className="icon"/>
-                        </button>
-                    </div>
-                </div>
+            <div data-aos="fade-up" className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+              alias, dolor odit assumenda voluptatum ratione ducimus veniam,
+              quam dolorum perferendis aliquid pariatur reiciendis eveniet
+              consectetur commodi fugit excepturi quod. Earum.
             </div>
-        </section>
-    )
-}
+
+            <div data-aos="fade-up" className="footerSocials">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            <div data-aos="fade-up" data-aos-duration="5000" className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME</small>
+            <small>COPYRIGHTS RESERVED - TECH 2022</small>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Footer;
--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.css";
-import {
-  HiOutlineClipboardCheck,
-  HiOutlineLocationMarker,
-} from "react-icons/hi";
+import { HiOutlineClipboardCheck, HiOutlineLocationMarker,} from "react-icons/hi";
+
 import img1 from "../../Assets/img1.jpg";
 import img2 from "../../Assets/img2.jpg";
 import img3 from "../../Assets/img3.jpg";
@@ -13,6 +11,9 @@ import img6 from "../../Assets/img6.jpg";
 import img7 from "../../Assets/img7.jpg";
 import img8 from "../../Assets/img8.jpg";
 import img9 from "../../Assets/img9.jpg";
+
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const Data = [
   {
@@ -26,91 +27,106 @@ const Data = [
       "The epitome of romance, Bora Bora is one of the best travel destination in the world. This place is known for its luxirious stays and adventerous activities.",
   },
   {
-      id:2,
-      imgsrc: img2,
-      destTitle: 'Machu Picchu',
-      location: 'Peru',
-      grade: 'CULTURAL RELAX',
-      fees: '$600',
-      description: 'Machu Picchu is both a cultural and natural UNESCO World Heritage Site. Since its rediscovery in 1911, growing numbers of tourists have visited the site each year, with numbers exceeding 1.4 million in 2017.'
+    id: 2,
+    imgsrc: img2,
+    destTitle: "Machu Picchu",
+    location: "Peru",
+    grade: "CULTURAL RELAX",
+    fees: "$600",
+    description:
+      "Machu Picchu is both a cultural and natural UNESCO World Heritage Site. Since its rediscovery in 1911, growing numbers of tourists have visited the site each year, with numbers exceeding 1.4 million in 2017.",
   },
   {
-      id:3,
-      imgsrc: img3,
-      destTitle: 'Great Barrier Reef',
-      location: 'Australia',
-      grade: 'CULTURAL RELAX',
-      fees: '$700',
-      description: 'The Great Barrier Reef is a site of remarkable variety and beauty on the north-east coast of Australia. It contains the world’s largest collection of coral reefs, with 1,500 species of fish.'
+    id: 3,
+    imgsrc: img3,
+    destTitle: "Great Barrier Reef",
+    location: "Australia",
+    grade: "CULTURAL RELAX",
+    fees: "$700",
+    description:
+      "The Great Barrier Reef is a site of remarkable variety and beauty on the north-east coast of Australia. It contains the world’s largest collection of coral reefs, with 1,500 species of fish.",
   },
   {
-      id:4,
-      imgsrc: img4,
-      destTitle: 'Cappadocia',
-      location: 'Turkey',
-      grade: 'CULTURAL RELAX',
-      fees: '$800',
-      description: 'Cappadocia is a land famous for its distinctive rock formation, historical heritage, and scenic hot air balloon trips.'
+    id: 4,
+    imgsrc: img4,
+    destTitle: "Cappadocia",
+    location: "Turkey",
+    grade: "CULTURAL RELAX",
+    fees: "$800",
+    description:
+      "Cappadocia is a land famous for its distinctive rock formation, historical heritage, and scenic hot air balloon trips.",
   },
   {
-      id:5,
-      imgsrc: img5,
-      destTitle: 'Guanajuato',
-      location: 'Mexico',
-      grade: 'CULTURAL RELAX',
-      fees: '$1100',
-      description: 'Guanajuato, the birthplace of famed muralist Diego Rivera, is also the site of Alhondiga de Ganaditas, a former town granary that became a revolutionary symbol after the heads of insurrectionists Hidalgo, Allende, Aldama and Jimenez were posted at the four corners of the building.'
+    id: 5,
+    imgsrc: img5,
+    destTitle: "Guanajuato",
+    location: "Mexico",
+    grade: "CULTURAL RELAX",
+    fees: "$1100",
+    description:
+      "Guanajuato, the birthplace of famed muralist Diego Rivera, is also the site of Alhondiga de Ganaditas, a former town granary that became a revolutionary symbol after the heads of insurrectionists Hidalgo, Allende, Aldama and Jimenez were posted at the four corners of the building.",
   },
   {
-      id:6,
-      imgsrc: img6,
-      destTitle: 'Cinque Terre',
-      location: 'Italy',
-      grade: 'CULTURAL RELAX',
-      fees: '$840',
-      description: 'The Cinque Terre are famous for their amazing natural landscape and colourful fishing villages perched on dramatic cliffs. UNESCO added the Cinque Terre to its list of World Heritage Sites because of their cultural landscape of great scenic and cultural value.'
+    id: 6,
+    imgsrc: img6,
+    destTitle: "Cinque Terre",
+    location: "Italy",
+    grade: "CULTURAL RELAX",
+    fees: "$840",
+    description:
+      "The Cinque Terre are famous for their amazing natural landscape and colourful fishing villages perched on dramatic cliffs. UNESCO added the Cinque Terre to its list of World Heritage Sites because of their cultural landscape of great scenic and cultural value.",
   },
   {
-      id:7,
-      imgsrc: img7,
-      destTitle: 'Angkor Wat',
-      location: 'Cambodia',
-      grade: 'CULTURAL RELAX',
-      fees: '$790',
-      description: 'Angkor Wat is an enormous Buddhist temple complex located in northern Cambodia. It was originally built in the first half of the 12th century as a Hindu temple.'
+    id: 7,
+    imgsrc: img7,
+    destTitle: "Angkor Wat",
+    location: "Cambodia",
+    grade: "CULTURAL RELAX",
+    fees: "$790",
+    description:
+      "Angkor Wat is an enormous Buddhist temple complex located in northern Cambodia. It was originally built in the first half of the 12th century as a Hindu temple.",
   },
   {
-      id:8,
-      imgsrc: img8,
-      destTitle: 'Taj Mahal',
-      location: 'India',
-      grade: 'CULTURAL RELAX',
-      fees: '$900',
-      description: 'The Taj Mahal is considered to be the greatest architectural achievement in the whole range of Indo-Islamic architecture. Its recognised architectonic beauty has a rhythmic combination of solids and voids, concave and convex and light shadow; such as arches and domes further increases the aesthetic aspect.'
+    id: 8,
+    imgsrc: img8,
+    destTitle: "Taj Mahal",
+    location: "India",
+    grade: "CULTURAL RELAX",
+    fees: "$900",
+    description:
+      "The Taj Mahal is considered to be the greatest architectural achievement in the whole range of Indo-Islamic architecture. Its recognised architectonic beauty has a rhythmic combination of solids and voids, concave and convex and light shadow; such as arches and domes further increases the aesthetic aspect.",
   },
   {
-      id:9,
-      imgsrc: img9,
-      destTitle: 'Bali Island',
-      location: 'Indonesia',
-      grade: 'CULTURAL RELAX',
-      fees: '$500',
-      description: 'Bali Indonesia. Also known as the Land of the Gods, Bali appeals through its sheer natural beauty of looming volcanoes and lush terraced rice fields that exude peace and serenity.'
-  }
+    id: 9,
+    imgsrc: img9,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali Indonesia. Also known as the Land of the Gods, Bali appeals through its sheer natural beauty of looming volcanoes and lush terraced rice fields that exude peace and serenity.",
+  },
 ];
 
 const Main = () => {
+
+  useEffect(() => {
+    Aos.init({duration: 1000})
+  }, [])
+
   return (
     <section className="main container section">
       <div className="secTitle">
-        <h3 className="title">Most visited destinations</h3>
+        <h3 data-aos="fade-right" className="title">Most visited destinations</h3>
       </div>
 
       <div className="secContent grid">
         {Data.map(
           ({ id, imgsrc, destTitle, location, grade, fees, description }) => {
             return (
-              <div className="singleDestination">
+              <div key={id}
+              data-aos="fade-up" 
+              className="singleDestination">
                 <div className="imageDiv">
                   <img src={imgsrc} alt={destTitle} />
                 </div>
